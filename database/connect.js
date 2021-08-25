428a067352fff13b324cd9ed76e17f08d4d861d7
zeherb <bouzid.zeher@gmail.com>
2021-08-25 19:08:13 +0100

    connect to database

---
+++ database/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+};
+
+mongoose
+    .connect("mongodb://localhost:27017/mydatabase", options)
+    .then((success) => {
+        console.log("=> Database connected successfully!");
+    })
+    .catch((error) => {
+        console.log("=> Database connected with errors!");
+    });
