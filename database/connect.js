f8cc6c97be43961be8e7524ef45605a378ae5bc3
zeherb <bouzid.zeher@gmail.com>
2021-08-26 01:05:18 +0100

    soliving connecting with database issues

--- database/connect.js
+++ database/connect.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+
 };
 
 mongoose
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+// connect to database 
+const connect = require('./database/connect');
 const app = express()
 // morgan config
 app.use(morgan('dev'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 // dotenv config
 require('dotenv').config()
 // port config
@@ -15,3 +22,14 @@ app.listen(port, () => {
     console.log(`Application listening at http://localhost:${port}`)
 })
 
+const userApi = require('./routes/userRoute')
+const authApi = require('./routes/authRoute')
+const eventApi = require('./routes/eventRoute')
+// const forgottenPasswordApi = require('./routes/forgottenPasswordRoute')
+const tagApi = require('./routes/tagRoute')
+
+app.use('', userApi)
+app.use('', authApi)
+app.use('', eventApi)
+// app.use('', forgottenPasswordApi)
+app.use('', tagApi)
