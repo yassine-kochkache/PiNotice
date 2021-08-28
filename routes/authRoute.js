dd4bf8767a72126b654cf869e876b3181c11cbe3
zeherb <bouzid.zeher@gmail.com>
2021-08-28 18:19:24 +0100

    auth with password hash

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,48 +1,88 @@
 const express = require('express');
-const passport = require('passport');
 const router = express.Router();
 const User = require('../models/userSchema')
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
 
 
 // login route
 
 router.post('/login', async (req, res) => {
 
-    const userDb = await User.findOne({ email: req.body.email.toLowerCase(), password: req.body.password });
-    if (userDb) {
-        // create jwt token
-        const tokenData = {
-            userId: userDb._id,
-            email: userDb.email,
-            firstName: userDb.firstName,
-            lastName: userDb.LastName,
-            role: userDb.role
+    try {
+        const userDb = await User.findOne({
+            email: req.body.email,
+        });
+        if (userDb) {
+            const result = await bcrypt.compare(req.body.password, userDb.password);
+            if (result) {
+                //create jwt token
+                const tokenData = {
+                    userId: userDb._id,
+                    email: userDb.email,
+                    firstName: userDb.firstName,
+                    lastName: userDb.lastName,
+                };
+                const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+                    expiresIn: process.env.JWT_EXPIRES_IN,
+                });
+                res.json({ token: token });
+            } else {
+                res.status(400).json({ message: "wrong credentials" });
+            }
+        } else {
+            res.status(400).json({ message: "wrong credentials" });
         }
-        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN });
-        res.json({ message: "Connected successfully", token: token })
-    } else {
-        res.status(400).json({ message: "Wrong credentials!" })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'internal server error' });
     }
+
+    // const userDb = await User.findOne({ email: req.body.email.toLowerCase(), password: req.body.password });
+    // if (userDb) {
+    //     // create jwt token
+    //     const tokenData = {
+    //         userId: userDb._id,
+    //         email: userDb.email,
+    //         firstName: userDb.firstName,
+    //         lastName: userDb.LastName,
+    //         role: userDb.role
+    //     }
+    //     const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN });
+    //     res.json({ message: "Connected successfully", token: token })
+    // } else {
+    //     res.status(400).json({ message: "Wrong credentials!" })
+    // }
 
 });
 
 // register route
 
 router.post("/register", async (req, res) => {
-    let userData = req.body
-    userData.email = userData.email.toLowerCase()
-    if (!("firstName" in userData && "lastName" in userData && "email" in userData && "password" in userData && "birthDate" in userData && "phone" in userData && "address" in userData)) {
-        res.status(400).json({ message: "Please fill your registration form" })
+    const userVerif = await User.findOne({ email: req.body.email.toLowerCase() })
+    if (userVerif) {
+        res.status(400).json({ message: "Email already used!" });
     } else {
-        const foundUser = await User.findOne({ email: userData.email })
-        if (foundUser) {
-            res.status(400).json({ message: "Email already exists" })
-        } else {
+        try {
+            const saltRounds = 10;
+            const hash = await bcrypt.hash(req.body.password, saltRounds);
+            const userData = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email.toLowerCase(),
+                password: hash,
+                birthDate: req.body.birthDate,
+                phone: req.body.phone,
+                address: req.body.address,
+                avatar: req.body.avatar
+            };
             const newUser = await User.create(userData)
-            res.status(200).json({ message: "Registred successfully" })
+            res.status(200).json({ message: "User created successfully", user: newUser })
+        } catch (err) {
+            res.status(400).json({ message: "erreur hasing password!" });
         }
     }
+
 });
 
 module.exports = router;
