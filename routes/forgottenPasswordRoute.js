d12277d7dc31ff00ad1a1e8f362c0db79a7cc86d
zeherb <bouzid.zeher@gmail.com>
2021-08-30 17:32:47 +0100

    forgotten password / reset password api completed

--- routes/forgottenPasswordRoute.js
+++ routes/forgottenPasswordRoute.js
@@ -16,37 +16,37 @@ router.post('/forgottenPassword', async (req, res) => {
     } else {
         const existToken = await Token.findOne({ userId: user._id });
         if (existToken) {
-            await Token.deleteOne()
-        } else {
+            await Token.findByIdAndDelete(existToken._id)
+        }
+        try {
+            // creating token
+            const saltRounds = 10;
+            const resetToken = crypto.randomBytes(32).toString("hex");
+            const hash = await bcrypt.hash(resetToken, saltRounds);
+            const tokenData = {
+                userId: user._id,
+                token: hash,
+                createdAt: Date.now(),
+            };
+            const newToken = await Token.create(tokenData)
+
+            const clientURL = 'localhost:3000'
+            const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
+
             try {
-                // creating token
-                const saltRounds = 10;
-                const resetToken = crypto.randomBytes(32).toString("hex");
-                const hash = await bcrypt.hash(resetToken, saltRounds);
-                const tokenData = {
-                    userId: user._id,
-                    token: hash,
-                    createdAt: Date.now(),
-                };
-                const newToken = await Token.create(tokenData)
+                const info = sendEmail(user.email, "Password Reset Request", { name: user.name, link: link, }, "./template/requestResetPassword.handlebars");
 
-                const clientURL = 'localhost:3000'
-                const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
-
-                try {
-                    const info = sendEmail(user.email, "Password Reset Request", { name: user.name, link: link, }, "./template/requestResetPassword.handlebars");
-
-                    res.json({ message: "email sent successfully" });
-                } catch (err) {
-                    console.log(err);
-                    res.status(500).json({ message: "email not sent" });
-                }
-
+                res.json({ message: "email sent successfully" });
             } catch (err) {
                 console.log(err);
-                res.status(500).json({ message: 'internal server error' });
+                res.status(500).json({ message: "email not sent" });
             }
-        };
+
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'internal server error' });
+        }
+
 
     }
 })
@@ -57,29 +57,31 @@ router.post('/resetPassword/', async (req, res) => {
     const saltRounds = 10;
     let passwordResetToken = await Token.findOne({ userId: req.body.userId });
     if (!passwordResetToken) {
-        throw new Error("Invalid or expired password reset token");
+        res.status(400).json({ message: "Invalid or expired password reset token" })
+    } else {
+        const isValid = await bcrypt.compare(req.body.token, passwordResetToken.token);
+        if (!isValid) {
+            res.status(400).json({ message: "Invalid or expired password reset token" })
+        } else {
+            try {
+                const hash = await bcrypt.hash(req.body.password, saltRounds);
+                const usertoUpdate = await User.findByIdAndUpdate(req.body.userId, { password: hash }, { new: true })
+                sendEmail(
+                    usertoUpdate.email,
+                    "Password Reset Successfully",
+                    {
+                        name: usertoUpdate.firstName,
+                    },
+                    "./template/resetPassword.handlebars"
+                );
+                await Token.findByIdAndDelete(passwordResetToken._id);
+                res.status(200).json({ message: "Password Reset Successfully" })
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({ message: 'internal server error' });
+            }
+        }
     }
-    const isValid = await bcrypt.compare(req.body.token, passwordResetToken.token);
-    if (!isValid) {
-        throw new Error("Invalid or expired password reset token");
-    }
-    const hash = await bcrypt.hash(req.body.password, saltRounds);
-    await User.updateOne(
-        { _id: req.body.userId },
-        { $set: { password: hash } },
-        { new: true }
-    );
-    const user = await User.findById({ _id: req.body.userId });
-    sendEmail(
-        user.email,
-        "Password Reset Successfully",
-        {
-            name: user.name,
-        },
-        "./template/resetPassword.handlebars"
-    );
-    await passwordResetToken.deleteOne();
-    res.status(200).json({ message: "Password Reset Successfully" })
 })
 
 
