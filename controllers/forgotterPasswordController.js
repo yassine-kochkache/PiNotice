53b00f480ab9ab32f4bf2903a80ebee1b174d852
zeherb <bouzid.zeher@gmail.com>
2021-09-22 15:23:06 +0100

    correcting some try catch block in code

--- controllers/forgotterPasswordController.js
+++ controllers/forgotterPasswordController.js
@@ -6,76 +6,60 @@ const sendEmail = require("../utils/email/sendEmail")
 
 // forgot password
 exports.forgotPassword = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email.toLowerCase() });
-    if (!user) {
-        res.status(400).json({ message: "This email address does not match any user" })
-    } else {
-        const existToken = await Token.findOne({ userId: user._id });
-        if (existToken) {
-            await Token.findByIdAndDelete(existToken._id)
-        }
-        try {
-            // creating token
-            const saltRounds = 10;
+    try {
+        const user = await User.findOne({ email: req.body.email.toLowerCase() });
+        if (!user) {
+            res.status(400).json({ message: "This email address does not match any user" })
+        } else {
+            const existToken = await Token.findOne({ userId: user._id });
+            if (existToken) {
+                await Token.findByIdAndDelete(existToken._id)
+            }
+            // creating new token
             const resetToken = crypto.randomBytes(32).toString("hex");
-            const hash = await bcrypt.hash(resetToken, saltRounds);
             const tokenData = {
                 userId: user._id,
-                token: hash,
+                token: resetToken,
                 createdAt: Date.now(),
             };
             const newToken = await Token.create(tokenData)
 
-            const clientURL = 'localhost:3000'
-            const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
+            const clientURL = process.env.CLIENT_URL
+            const link = `${clientURL}/#/reset-password/${resetToken}`;
 
-            try {
-                const info = sendEmail(user.email, "Password Reset Request", { name: user.name, link: link, }, "./template/requestResetPassword.handlebars");
+            const info = sendEmail(user.email, "Password Reset Request", { name: user.name, link: link, }, "./template/requestResetPassword.handlebars");
+            res.json({ message: "email sent successfully" });
 
-                res.json({ message: "email sent successfully" });
-            } catch (err) {
-                console.log(err);
-                res.status(500).json({ message: "email not sent" });
-            }
-
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: 'internal server error' });
         }
-
-
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'internal server error' });
     }
 }
 
 //reset password
 
 exports.resetPassword = async (req, res) => {
-    const saltRounds = 10;
-    let passwordResetToken = await Token.findOne({ userId: req.body.userId });
-    if (!passwordResetToken) {
-        res.status(400).json({ message: "Invalid or expired password reset token" })
-    } else {
-        const isValid = await bcrypt.compare(req.body.token, passwordResetToken.token);
-        if (!isValid) {
+    try {
+        const passwordResetToken = await Token.findOne({ token: req.body.token });
+        if (!passwordResetToken) {
             res.status(400).json({ message: "Invalid or expired password reset token" })
         } else {
-            try {
-                const hash = await bcrypt.hash(req.body.password, saltRounds);
-                const usertoUpdate = await User.findByIdAndUpdate(req.body.userId, { password: hash }, { new: true })
-                sendEmail(
-                    usertoUpdate.email,
-                    "Password Reset Successfully",
-                    {
-                        name: usertoUpdate.firstName,
-                    },
-                    "./template/resetPassword.handlebars"
-                );
-                await Token.findByIdAndDelete(passwordResetToken._id);
-                res.status(200).json({ message: "Password Reset Successfully" })
-            } catch (err) {
-                console.log(err);
-                res.status(500).json({ message: 'internal server error' });
-            }
+            const hash = await bcrypt.hash(req.body.password, 10);
+            const usertoUpdate = await User.findByIdAndUpdate(passwordResetToken.userId, { password: hash }, { new: true })
+            const info = sendEmail(
+                usertoUpdate.email,
+                "Password Reset Successfully",
+                {
+                    name: usertoUpdate.firstName,
+                },
+                "./template/resetPassword.handlebars"
+            );
+            await Token.findByIdAndDelete(passwordResetToken._id);
+            res.status(200).json({ message: "Password Reset Successfully" })
         }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'internal server error' });
     }
 }
--- models/userSchema.js
+++ models/userSchema.js
@@ -2,26 +2,34 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
     events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
     role: { type: String, default: 'user' },
     birthDate: {
         type: Date, validate: {
             validator: function (validation) {
-                validation.setFullYear(validation.getFullYear() + 16)
                 const currentTime = new Date();
-                currentTime.setHours(0, 0, 0, 0);
-                return validation.getTime() <= currentTime.getTime();
+                const curretYear = currentTime.getFullYear()
+                const currentMonth = currentTime.getMonth()
+                const currentDay = currentTime.getDate()
+                const ageYear = validation.getFullYear() + 16
+                const ageMonth = validation.getMonth()
+                const ageDay = validation.getDate()
+                if (curretYear >= ageYear && currentMonth >= ageMonth && currentDay >= ageDay) {
+                    return true
+                } else {
+                    return false;
+                }
             },
-            message: props => 'You must be at least 16 years old.'
-        }
+            message: "You must be at least 16 years old"
+        }, required: true
     },
-    phone: String,
-    address: String,
-    avatar: String
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    avatar: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
