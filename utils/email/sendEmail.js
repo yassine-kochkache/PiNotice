db8456da954e11d31e8ba9c68488ab2cda936882
zeherb <bouzid.zeher@gmail.com>
2021-10-04 14:56:58 +0100

    options email

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -18,18 +18,16 @@ const sendEmail = (email, subject, payload, template, attachments = []) => {
 
       const source = fs.readFileSync(path.join(__dirname, template), "utf8");
       const compiledTemplate = handlebars.compile(source);
-      const options = () => {
-        return {
-          from: process.env.MAIL,
-          to: email,
-          subject: subject,
-          html: compiledTemplate(payload),
-          attachments: attachments
-        };
+      const options = {
+        from: process.env.MAIL,
+        to: email,
+        subject: subject,
+        html: compiledTemplate(payload),
+        attachments: attachments
       };
 
       // Send email
-      const info = await transporter.sendMail(options())
+      const info = await transporter.sendMail(options)
       resolve(info)
     } catch (error) {
       console.log(error);
